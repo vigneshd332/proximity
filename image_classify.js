b37315c90d5c18be6fbfd85ea11cff5216dfa68c
Vignesh D <vigneshd332@gmail.com>
2019-12-22 23:10:40 +0530

    Add files via upload

---
+++ image_classify.js
@@ -0,0 +1,100 @@
+const watson = require('watson-developer-cloud');
+
+//object containing credentials such as api key and tokens
+const credential = {
+    url: 'https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/b79e80d4-d653-4713-8abf-c629b018cba5',
+    version: '2018-03-19',
+    iam_apikey: process.env.watson_key
+}
+
+const ir = new watson.VisualRecognitionV3(credential);
+
+module.exports = {
+    //Function that checks if it is an image and returns the image. If it is not an image then it returns false
+    isImage : (msg) => {
+        try {
+            //if msg doesnt have a url then this object will not exist and therefore return an error
+            const image = msg.attachments.array()[0].url; //json object
+            console.log("image url is " + image);
+            
+            //parameters for the general classification 
+            const params_gen = {
+                url: msg.attachments.array()[0].url,
+                classifier_ids: "default",
+            };
+           
+            //paramters for the food classification
+           const params_food = {
+                url: msg.attachments.array()[0].url,
+               classifier_ids: "food"
+           } 
+
+           //first promise that does the first classification
+           let prom1 = new Promise(
+               (resolve, reject) => {
+                //classify object contains image recognition object with general classification
+                let classify_object = null;
+                ir.classify(params_gen , (err, res) => {   
+                    if(err){
+                        console.log(err);
+                        reject(err);
+                    }
+                    else{
+                        classify_object = res; 
+                        resolve(classify_object); //first promise resolves a raw classify object
+                    }
+                });
+               }
+           );
+
+       
+           //Function used to check if the image contains food. Takes raw classify object as a parameter
+           function check_image(image_object){
+                for(item of image_object.images[0].classifiers[0].classes){
+                    if(item.class === 'food' ){
+                        console.log("It is a food Item")
+                        return true;
+                    }
+               }
+               console.log("it is a general item")
+               return false;
+           }
+
+                let promise = prom1.then(function(result){
+                //check if result contains food or person    
+                let bool = check_image(result);
+
+                //if it is a food image than bool will be false and it will return a new promise
+                if(bool == true){
+                    return new Promise((resolve, reject) => {
+                        ir.classify(params_food, 
+                            (err ,res) => {
+                                if(err){
+                                    reject(err);
+                                }
+                                else{
+                                    resolve(res); //passes raw classify object
+                                }
+                            });
+                    });
+                }
+               else{
+                   return (new Promise(
+                       (resolve, reject) => {
+                           resolve(result);
+                       }));
+               }
+           }).catch(err => console.log(err));
+
+           return(promise);
+        }
+        catch (TypeError) {
+            //returns new promise with value of the text from the message
+            console.log("Type Error caught")
+            return (new Promise(
+                (resolve, reject) => {
+                   resolve(msg.content); 
+                }))
+        }
+    }
+}
--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const cheerio = require("cheerio");
 const request = require("request");
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
-var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const watson = require('watson-developer-cloud');
 var fs = require('fs');
+const image_classify = require('./image_classify');
+const print = require('./print');
 
 
 const client = new Discord.Client();
 
 const queue = new Map();
 
-/*client.once('ready', () => {
+client.once('ready', () => {
 	console.log('Ready!');
-});*/
-client.on("ready", () => {
-	/* Log */ console.log("Blyatman is now up and running!");
-	client.user.setActivity('image databases', { type: 'WATCHING' })
-	.then(presence => 
-		/* Log */ console.log(`Status updated succesfully.\n==================================================`))
-  	.catch("There was an error updating the status");
 });
 
 client.once('reconnecting', () => {
@@ -27,6 +22,29 @@ client.once('reconnecting', () => {
 
 client.once('disconnect', () => {
 	console.log('Disconnect!');
+});
+client.on('message' , msg =>{
+    //If the message is from the bot then do nothing
+    if(msg.author.bot) return;
+    else{
+        //checks the image and returns a promise.
+        const image = image_classify.isImage(msg); //image_check is a function requied from another file
+        image.then(
+            (value) => {
+            //checks the type of the promis value. If it is a stirng then run the first block. 
+            if(typeof(value) == "string"){
+                console.log("it is a string")
+                //msg.reply("it is not an image");
+                //insert watson conversation here
+            }
+            else{
+                let replyMessage = print.getMessage(value);
+                msg.reply(replyMessage);
+            }
+               console.log(value);
+        });
+
+    }
 });
 client.on("message", function(message) {
  
@@ -41,88 +59,7 @@ client.on("message", function(message) {
     }
  
 });
-client.on("message", (message) => {
-	if (message.content.startsWith(`${process.env.prefix}ir`)) {
-	  var args = message.content.substring(1).split(' ')[1];
 
-	  try {
-		  if (!args.includes("grabify")){
-			  if(args.includes("http") || args.includes("www") && ((args.includes("jpeg") || args.includes("jpg") || args.includes("png") || args.includes("image")))) {
-				  /* Log */ console.log("Valid image found.");
-					 message.channel.startTyping(3);
-				  message.channel.send("This seems legit. Lemme analyze it with my 3 human neurons...")
-				  var imageID = Math.floor(Math.random()*9000000000) + 1000000000;
-				  /* Log */ console.log("Generated random ID for the image. (" + imageID + ")")
-				  var request = require('request');
-				  const stream = request(args).pipe(fs.createWriteStream("./temp/" + imageID + '.png'))
-				  stream.on('finish', () => {
-					console.log("Image downloaded.\nRunning IBM's API.");
-					  var visualRecognition = new VisualRecognitionV3({
-						  url: 'https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/b79e80d4-d653-4713-8abf-c629b018cba5',
-							 version: '2018-03-19',
-						  iam_apikey: process.env.watson_key
-					  });
-						var images_file= fs.createReadStream("./temp/" + imageID + ".png");
-					  var params = {
-						  images_file: images_file,
-						  threshold: 0.5
-					  };
-					  visualRecognition.classify(params, function(err, response) {
-						  if (err){
-							  /* Log */ console.log("Image recognition failed!");
-						  }
-						  else{
-							  var stringResult = (JSON.stringify(response.images[0].classifiers[0].classes[0].class, null, 2)).replace(/"/g,"");
-							  /* Log */ console.log("Prediction: " + stringResult);
-							  switch(stringResult.charAt(0)){
-								  case "a": case "e": case "i": case "o": case "u":
-									  message.channel.send("Okay, my advanced mind thinks it is an " + stringResult);
-									  break;
-								  default:
-									  message.channel.send("Okay, my advanced mind thinks it is a " + stringResult);
-									  break;
-							  }
-							  /* Log */ console.log("==================================================")
-						  }		
-					  });
-					  fs.unlink("./temp/" + imageID + ".png", (err) => {
-						  if (err) {
-							  /* Log */ console.log("There was an error deleting the image :(");
-						  } else {
-							  /* Log */ console.log('Image deleted succesfully!');                                
-						  }
-						  message.channel.stopTyping(true);
-					  });
-				  });	
-			  } else if (args.includes("shut") || args.includes("fuck") || args.includes("ass") || args.includes("nigger") || args.includes("bitch") || args.includes("kid") || args.includes("retard")){
-				  message.guild.fetchMember(message.author).then(member => {
-					  message.channel.send("you nigger have big gay " + member, {
-						 // file: "./content/boi.mp3"
-					  });
-					});		
-			  } 
-			  else if (args.includes("http") || args.includes("www") && !(args.includes("jpeg") || args.includes("jpg") || args.includes("png") || args.includes("image"))){
-				  message.channel.send("Your link is broke as fuck bro");
-			  }  
-			  else {
-				  message.channel.send("wtf I think someone just mistook me for Google Assistant", {
-					  //file: "./content/wat.png"
-				  });
-				  setTimeout(function () {
-					  message.channel.send("bitch");
-				  }, 2500);
-			  }     
-		  }
-		  else
-		  {
-			  message.channel.send("good try");
-		  }
-	  }
-	  catch (err){
-		  message.channel.send("Oof, I crashed");
-	  } 
-  }
-});
 client.on('message', async message => {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(process.env.prefix)) return;
