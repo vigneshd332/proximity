8a7e372ce770c47ccdf9c0e8797a6e4ab614a0ef
Vignesh D <vigneshd332@gmail.com>
2020-06-07 18:21:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,10 +57,53 @@ client.on('message' , msg =>{
 
     }
 });
+client.on('message', (message) => {
+
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(`${process.env.prefix}gif`))
+  {
+    // Split message to search GIPHY
+    let splitWord = message.toString().split(" ");
+    let gifWord   = "";
+
+    // Loop through incase of multiple word search
+    for( var i = 1; i < splitWord.length; i++)
+    {
+      if(i > 1)
+      {
+        gifWord = gifWord + "+";
+      }
+
+      gifWord = gifWord + splitWord[i];
+    }
+
+    request("http://api.giphy.com/v1/gifs/search?q=" + gifWord + "&api_key=" + process.env.giphykey + "&limit=100", function (error, response, body)
+    {
+      if (!error && response.statusCode == 200)
+      {
+        // Convert body to JSON object
+        let jsonUrl = JSON.parse(body);
+
+        // Get random number to choose GIF
+        let totGif = jsonUrl.data.length;
+
+        if(totGif > 100)
+        {
+          totGif = 100;
+        }
+
+        let ranNum = Math.floor(Math.random() * totGif);
+
+        message.channel.sendMessage(jsonUrl.data[ranNum].url);
+      }
+    });
+  }
+});
 client.on('message', message => {
   if (awaiting.includes(message.author.id)) return;
 	message.toString().toLowerCase();
-	if ( message.toString().toLowerCase().includes("nigger") || message.toString().toLowerCase().includes("nigga") || message.toString().toLowerCase().includes("fag") || message.toString().toLowerCase().includes("faggot") || message.toString().toLowerCase().includes("nigguh")) {
+	if ( message.toString().toLowerCase().includes("nigger") || message.toString().toLowerCase().includes("nigga") || message.toString().toLowerCase().includes("fag") || message.toString().toLowerCase().includes("faggot") || message.toString().toLowerCase().includes("nigguh")|| message.toString().toLowerCase().includes("niggur")) {
 		message.delete(10);
 		message.channel.send("No slurs, " + message.author )
 	     
